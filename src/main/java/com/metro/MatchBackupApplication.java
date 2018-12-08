40bfcdea22efec9bc9529138f1251b5448bd3d08
wang.wei <eric218@163.com>
2018-12-08 20:24:32 +0800

    add

---
+++ src/main/java/com/metro/MatchBackupApplication.java
@@ -0,0 +1,12 @@
+package com.metro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchBackupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchBackupApplication.class, args);
+    }
+}
